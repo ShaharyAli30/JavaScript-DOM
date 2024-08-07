80c9d5a9203c44b15566d524b458eb82ba3df124
Shaharyar Ali <110044492+ShaharyAli30@users.noreply.github.com>
2024-08-07 16:33:04 +0500

    Add files via upload

---
+++ multiple-elements.js
@@ -0,0 +1,7 @@
+//selecting a multiple Element in Javascript Dom.
+var c =document.querySelectorAll("h1")
+console.log(c);
+//By using a For each represent multiple elements one y one
+forEach(function() {
+  console.log(e);      
+});
